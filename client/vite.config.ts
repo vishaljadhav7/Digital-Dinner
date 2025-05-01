2c8fcf1c72e28fe81dd627869b0c145e549f56c8
vishaljadhav7 <vishalj.7661@gmail.com>
2025-05-01 17:35:03 +0530

    did some configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()  
+    react()
   ],
+  build: {
+    outDir: 'dist',
+  },
+  base: '/'
 })

ea3556c2dffbae5718d995c5fbfde3908ef7f440
vishaljadhav7 <vishalj.7661@gmail.com>
2025-05-01 17:53:06 +0530

    handled cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" , "https://digital-dinner-seven.vercel.app"],
     methods: ["GET", "POST"], 
     credentials: true 
   }));
